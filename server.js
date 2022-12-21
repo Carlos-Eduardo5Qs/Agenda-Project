0246b2c344cedbcf3511a2d5d65d3cfc1ea56046
Carlos-Eduardo5Qs <carloseduardorm@outlook.com>
2022-12-21 20:50:39 -0300

    Subindo arquivos com o problema do arquivo EJS solucionado

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const PORT = 3000
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join('public')));
+app.use(express.static(path.resolve(__dirname,'public')));
 app.use(route);
 
-app.set('views', path.join('src', 'views'));
+app.set('views', path.resolve(__dirname,'src', 'views'));
 app.set('views engine', 'ejs');
 
 app.listen(PORT, (err) => {
